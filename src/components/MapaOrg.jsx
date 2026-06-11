c3be9b60f300bd3ba2bd7aa66824e2c926a63b28
AngeloPc <angelomelo1999@gmail.com>
2026-06-11 11:53:54 -0500

    feat(MapaOrg): mejora el estilo del componente

--- src/components/MapaOrg.jsx
+++ src/components/MapaOrg.jsx
@@ -2,24 +2,29 @@ import './MapaOrg.scss'
 
 export function MapaOrg(){
     return(
+        <>
+        <h2>Ornanigrama</h2>
         <div class="organigrama">
-    <div class="nivel">
-        <div class="cargo">Product Owner</div>
-    </div>
+            <div class="nivel">
+                <div class="cargo">Product Owner :Melo Mayta Angelo</div>
+            </div>
 
-    <div class="linea"></div>
+            <div class="linea"></div>
 
-    <div class="nivel">
-        <div class="cargo">Scrum Master</div>
-    </div>
+            <div class="nivel">
+                <div class="cargo">Scrum Master :Huarac Valle Manuel Angel</div>
+            </div>
 
-    <div class="linea"></div>
+            <div class="linea"></div>
 
-    <div class="equipo">
-        <div class="cargo">Frontend</div>
-        <div class="cargo">Backend</div>
-        <div class="cargo">QA Tester</div>
-    </div>
-</div>
+            <div class="equipo">
+                <div class="cargo">Developer : León Mejia Jhassir</div>
+                <div class="cargo">Developer : Quispe Orihuela Fernando Gabriel </div>
+                <div class="cargo">Developer : Llacctahuaman David</div>
+                <div class="cargo">Developer : Quispe Orihuela Fernando Gabriel </div>
+            </div>
+        </div>
+        </>
+        
     )
 }
